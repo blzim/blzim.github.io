f1e8cd4d8cd132806148962d91342e2407542129
Brian Zimmerman <brian_zimmerman@yahoo.com>
2017-11-01 12:47:08 -0700

    linux 11 to 20

--- LinuxQuiz/randomPage.js
+++ LinuxQuiz/randomPage.js
@@ -1,5 +1,5 @@
 function randomlinks(){
-    var myrandom=Math.round(Math.random()*9)
+    var myrandom=Math.round(Math.random()*20)
     var links=new Array()
 	links[0]="linux000.html"
 	links[1]="linux001.html"
@@ -12,5 +12,15 @@ function randomlinks(){
 	links[8]="linux008.html"
 	links[9]="linux009.html"
 	links[10]="linux010.html"
+	links[11]="linux011.html"
+	links[12]="linux012.html"
+	links[13]="linux013.html"
+	links[14]="linux014.html"
+	links[15]="linux015.html"
+	links[16]="linux016.html"
+	links[17]="linux017.html"
+	links[18]="linux018.html"
+	links[19]="linux019.html"
+	links[20]="linux020.html"
     window.location=links[myrandom]
 }
